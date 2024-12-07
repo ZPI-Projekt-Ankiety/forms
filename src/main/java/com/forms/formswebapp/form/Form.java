1b97775ce0ed37a46d9370ca987f80de259b47bb
RefubuS <refubuspl@gmail.com>
2024-12-07 23:33:46 +0100

    Remove unused import

--- src/main/java/com/forms/formswebapp/form/Form.java
+++ src/main/java/com/forms/formswebapp/form/Form.java
@@ -1,7 +1,6 @@
 package com.forms.formswebapp.form;
 
 import lombok.Builder;
-import lombok.Getter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
