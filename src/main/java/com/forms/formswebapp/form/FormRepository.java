84856ed217c585edcb03ba49b7977e8f9e2f4c2f
Michauuek <michal.rzemieniuk@gmail.com>
2024-12-07 11:19:17 +0100

    Added closing scheduler

--- src/main/java/com/forms/formswebapp/form/FormRepository.java
+++ src/main/java/com/forms/formswebapp/form/FormRepository.java
@@ -2,5 +2,11 @@ package com.forms.formswebapp.form;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface FormRepository extends MongoRepository<Form, String> {
+import java.time.LocalDateTime;
+import java.util.List;
+
+interface FormRepository extends MongoRepository<Form, String> {
+
+    List<Form> findByClosingTimeBeforeAndStatusNot(final LocalDateTime closingTime, final Form.Status status);
+
 }
